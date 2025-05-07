5f456ff2c717a1f81eaa805a47762a6d795c7e85
Roy <inf.roycd@gmail.com>
2025-05-07 13:54:23 -0500

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,6 @@ const app = express()
 const PORT = process.env.PORT || 8000
 
 app.use(cors())
-app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
