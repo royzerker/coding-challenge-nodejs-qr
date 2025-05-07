bf4f525bced94e46d5755e88ce20ca4a06f763bb
Roy <inf.roycd@gmail.com>
2025-05-07 02:23:32 -0500

    chore: update

--- dist/services/qr.service.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// import { UserService } from "./UserService";
-// import { UserRepository } from "./UserRepository";
-// const mockUserRepo = {
-//   getAll: jest.fn(),
-// };
-// const userService = new UserService(mockUserRepo);
-// userService.getUserData();
-// expect(mockUserRepo.getAll).toHaveBeenCalled();
